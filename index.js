c733a4442957c439491e31625d30bbd1de1bddcb
EffiEmmnanuel <effiemmanuel.jobs@gmail.com>
2025-03-17 05:49:12 +0100

    Added send-sms route

--- index.js
+++ index.js
@@ -20,25 +20,38 @@ async function sendSMS() {
 }
 
 // Schedule the task to run every hour
-cron.schedule("0 * * * *", async () => {
-  console.log("Running SMS cron job at", new Date());
-  await sendSMS();
-});
+// cron.schedule("0 * * * *", async () => {
+//   console.log("Running SMS cron job at", new Date());
+//   await sendSMS();
+// });
 
 // Keep the script running
-console.log("Cron job started. Will run every hour.");
+// console.log("Cron job started. Will run every hour.");
 
 // Express server for health checks
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-  res.send("Cron job is running.");
+  res.send("TLCC SMS job is live and running.");
 });
 
 app.get("/ping", (req, res) => {
   console.log("Server is live.");
   res.send("Server is live.");
+});
+
+app.get("/send-sms", async (req, res) => {
+  console.log("Running sms sender...");
+  try {
+    await sendSMS();
+    return res.status(200).json({ message: "SMS job has finished running🎉" });
+  } catch (error) {
+    return res.status(500).json({
+      message:
+        "An error occured while we processed your request. Please, try again.",
+    });
+  }
 });
 
 app.listen(PORT, () => {
